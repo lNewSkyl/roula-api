1b5d7fa8896ad06671b2400a675d9fa79b190c29
lNewSkyl <atom-1121@mail.ru>
2022-02-21 15:03:57 +0300

    add devise auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users, controllers: { registrations: "registrations" }
+  root "home#homepage"
+  get 'homepage', to: 'home#homepage'
+  get 'dashboard', to: 'home#dashboard'
 end
---
+++ db/migrate/20220221114349_add_username_to_users.rb
@@ -0,0 +1,6 @@
+class AddUsernameToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :balance, :float, default: 0
+  end
+end
