ccabfec0f697ea3a61766f84538c7cfb5431ce1e
lNewSkyl <atom-1121@mail.ru>
2022-02-21 16:08:20 +0300

    user show and user_index pages. some routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
+
+  namespace :api do
+    namespace :v1 do
+    end
+  end
   root "home#homepage"
   get 'homepage', to: 'home#homepage'
   get 'dashboard', to: 'home#dashboard'
+  resources :users do
+  end
 end
