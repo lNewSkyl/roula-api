0b3af5f4040e0b6b7c79aff6dac0a72774c164ec
lNewSkyl <atom-1121@mail.ru>
2022-02-21 17:04:52 +0300

    add devise view pages, username editing, some links

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_action :configure_account_update_params, only: [:update]
   private
+
+  def after_update_path_for(resource)
+    root_path
+  end
 
   def after_sign_up_path_for(resource)
     root_path
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end
